ed7d13d5a457f0cb435841f6136df17d2834ea72
ch-study-account <ch.harada008@gmail.com>
2021-09-06 12:18:45 +0900

    初期セットアップ（テンプレート登録）

---
+++ diary/views.py
@@ -0,0 +1,12 @@
+from re import template
+from typing import Generator
+from django.shortcuts import render
+
+from django.views import generic
+# Create your views here.
+
+def index(request):
+    return render(request, 'diary/index.html')
+
+class IndexView(generic.TemplateView):
+    template_name = "diary/index.html"
