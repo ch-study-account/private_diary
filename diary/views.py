58bd16eae3f2c5099878cf20152f22e39b9b4031
ch-study-account <ch.harada008@gmail.com>
2021-09-07 12:33:11 +0900

    お問い合わせフォーム作成

--- diary/views.py
+++ diary/views.py
@@ -1,12 +1,15 @@
-from re import template
-from typing import Generator
 from django.shortcuts import render
 
 from django.views import generic
 # Create your views here.
+from .froms import InquiryForm
 
 def index(request):
     return render(request, 'diary/index.html')
 
 class IndexView(generic.TemplateView):
     template_name = "diary/index.html"
+
+class InquiryView(generic.FormView):
+    template_name = 'diary/inquiry.html'
+    from_class = InquiryForm
